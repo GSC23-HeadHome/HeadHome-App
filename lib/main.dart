8df815a2d002d65460ff3e0806feb74988ecfa63
Trigon25 <marccherndiyong2528@gmail.com>
2023-03-04 21:52:41 +0800

    Added auth pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:headhome/pages/authlogin.dart';
+import 'package:headhome/pages/authregister.dart';
 import './pages/caregiver.dart' show Caregiver;
 import './pages/patient.dart' show Patient;
 import './pages/volunteer.dart' show Volunteer;
@@ -134,6 +136,23 @@ class _MyHomePageState extends State<MyHomePage> {
                   );
                 },
                 child: Text('Volunteer Page')),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const AuthRegister()),
+                  );
+                },
+                child: Text('Register Page')),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const AuthLogin()),
+                  );
+                },
+                child: Text('Login Page')),
           ],
         ),
       ),
