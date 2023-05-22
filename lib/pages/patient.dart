ad270ec197c6004a82359abbac4287dd6d801996
Trigon25 <marccherndiyong2528@gmail.com>
2023-05-22 22:04:55 +0800

    updated singlechildscrollview to enable directions scroll

--- lib/pages/patient.dart
+++ lib/pages/patient.dart
@@ -628,16 +628,17 @@ class _PatientState extends State<Patient> {
     }
   }
 
-  // Contact of the caregiver
+  /// Contact of the caregiver
   void _getContact(cgId) async {
     debugPrint("Getting Contact");
     _cgcontactnumModel = await ApiService.getCgContact(cgId, crId);
+    debugPrint(_cgcontactnumModel!.cgContactNum);
     setState(() {
       priContactNo = _cgcontactnumModel!.cgContactNum;
     });
   }
 
-  // Editing the profile information of the patient
+  /// Editing the profile information of the patient
   void _updateData() async {
     var response = await ApiService.updateCarereceiver(crId, nameValue,
         homeAddress, phoneNumberValue, priContactUsername, priContactRel);
@@ -654,7 +655,7 @@ class _PatientState extends State<Patient> {
     }
   }
 
-  // Enable direct calling of caregiver by patient
+  /// Enable direct calling of caregiver by patient
   _callNumber() async {
     bool? res = await FlutterPhoneDirectCaller.callNumber(
         priContactNo.replaceAll(' ', ''));
@@ -664,10 +665,10 @@ class _PatientState extends State<Patient> {
       debugPrint("Not working");
     }
   }
-  // ------- END OF PROFILE METHODS -------
+  /// ------- END OF PROFILE METHODS -------
 
-  // ------- START OF FUNCTIONAL LOCATION METHODS -------
-  // Updates current position of the patient to the database
+  /// ------- START OF FUNCTIONAL LOCATION METHODS -------
+  /// Updates current position of the patient to the database
   Future<String> _updateLocStatus(bool manualCall) async {
     if (currentPosition != null) {
       double distFromSafe = Geolocator.distanceBetween(
@@ -693,8 +694,8 @@ class _PatientState extends State<Patient> {
     }
   }
 
-  // CAN ALSO USE THIS FUNCTION TO CALL FOR BLUETOOTH BUTTON PRESS BY
-  // manualCALL = true WHEN CALLING THE FUNCTION
+  /// CAN ALSO USE THIS FUNCTION TO CALL FOR BLUETOOTH BUTTON PRESS BY
+  /// manualCALL = true WHEN CALLING THE FUNCTION
   void _locStatusCallHelp(bool manualCall) async {
     String locStatus = await _updateLocStatus(manualCall);
     debugPrint(locStatus);
@@ -708,7 +709,7 @@ class _PatientState extends State<Patient> {
     }
   }
 
-  // Timer for location update
+  /// Timer for location update
   Future<void> _locationHandler() async {
     _locStatusCallHelp(false);
     _lTimer = Timer.periodic(const Duration(minutes: 1), (timer) async {
@@ -717,7 +718,7 @@ class _PatientState extends State<Patient> {
   }
 
   void _requestHelp() async {
-    // Position position = await _getCurrentPosition();
+    /// Position position = await _getCurrentPosition();
     debugPrint(currentPosition!.latitude.toString());
     debugPrint(currentPosition!.longitude.toString());
     var response = await ApiService.requestHelp(
@@ -728,12 +729,12 @@ class _PatientState extends State<Patient> {
     Map<String, dynamic> res = json.decode(response.body);
 
     _processRouteResponse(res);
-    // Calling route api every 5 mins
+    /// Calling route api every 5 mins
     _routingTimer();
   }
 
   void _processRouteResponse(Map<String, dynamic> res) {
-    // Converting polyline
+    /// Converting polyline
     Set<Polyline> tempPoly = {};
     for (int i = 0; i < res["Route"].length; i++) {
       PolylinePoints polylinePoints = PolylinePoints();
@@ -771,7 +772,7 @@ class _PatientState extends State<Patient> {
     });
   }
 
-  // Timer to update route of patient
+  /// Timer to update route of patient
   Future<void> _routingTimer() async {
     _rTimer = Timer.periodic(const Duration(minutes: 5), (timer) async {
       debugPrint("Routing");
@@ -784,9 +785,8 @@ class _PatientState extends State<Patient> {
     });
   }
 
-  // Update routing
+  /// Update routing
   void _routingHelp() async {
-    // Position position = await _getCurrentPosition();
     var response = await ApiService.routingHelp(
         currentPosition!,
         widget.carereceiverModel.safezoneCtr.lat.toString(),
@@ -891,7 +891,8 @@ class _PatientState extends State<Patient> {
                 ),
               ),
             ),
-            Visibility( // Bar with directions to map home
+            Visibility(
+              /// Bar with directions to map home
               visible: sosCalled,
               child: Container(
                 width: double.infinity,
@@ -908,6 +909,7 @@ class _PatientState extends State<Patient> {
                 ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
                   children: [
                     Icon(
                       determineRouteArrow(
@@ -916,51 +918,42 @@ class _PatientState extends State<Patient> {
                               : routeLogsModel[routeIndex + 1]),
                       size: 100,
                     ),
-                    Flexible(
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    SizedBox(
+                      width: 300,
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        physics: const ClampingScrollPhysics(),
                         child: Text.rich(
-                          TextSpan(
-                            children: [
-                              TextSpan(
-                                text: "${parseHTML(
-                                    routeIndex >= routeLogsModel.length - 1
-                                        ? "Continue to destination"
-                                        : routeLogsModel[routeIndex + 1]
-                                            .htmlInstructions)}\n",
-                              ),
-                              TextSpan(
-                                  text:
-                                      routeIndex >= routeLogsModel.length - 1
-                                          ? "For "
-                                          : routeLogsModel[routeIndex + 1]
-                                                      .maneuver ==
-                                                  "straight"
-                                              ? "For "
-                                              : "In "),
-                              TextSpan(
-                                text:
-                                    '${distanceToNextRouteLog.toInt().toString()}m',
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                            style: const TextStyle(fontSize: 20)
-                          ),
-                          textAlign: TextAlign.justify,
+                          TextSpan(children: [
+                            TextSpan(
+                              text:
+                                  "${parseHTML(routeIndex >= routeLogsModel.length - 1 ? "Continue to destination" : routeLogsModel[routeIndex + 1].htmlInstructions)}\n",
+                            ),
+                            TextSpan(
+                                text: routeIndex >= routeLogsModel.length - 1
+                                    ? "For "
+                                    : routeLogsModel[routeIndex + 1].maneuver ==
+                                            "straight"
+                                        ? "For "
+                                        : "In "),
+                            TextSpan(
+                              text:
+                                  '${distanceToNextRouteLog.toInt().toString()}m',
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ], style: const TextStyle(fontSize: 20)),
                         ),
                       ),
                     ),
-                    const SizedBox(
-                      width: 10,
-                    )
                   ],
                 ),
               ),
             )
           ],
         ),
-        Align( // Floating red "Navigate Home" button
+        Align(
+          /// Floating red "Navigate Home" button
           alignment: Alignment.center,
           child: Visibility(
             visible: visible,
@@ -1022,9 +1015,9 @@ class _PatientState extends State<Patient> {
           width: 80,
           child: FittedBox(
             child: FloatingActionButton(
-              //Floating action button on Scaffold
+              /// Floating action button on Scaffold
               onPressed: () {
-                //code to execute on button press
+                /// code to execute on button press
                 debugPrint("Calling caregiver");
                 _callNumber();
               },
@@ -1035,12 +1028,12 @@ class _PatientState extends State<Patient> {
           )),
       floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
       bottomNavigationBar: BottomAppBar(
-        //bottom navigation bar on scaffold
+        /// bottom navigation bar on scaffold
         height: 100,
         color: Theme.of(context).colorScheme.tertiary,
         shape: const CircularNotchedRectangle(), //shape of notch
         notchMargin:
-            5, //notche margin between floating button and bottom appbar
+            5, /// notche margin between floating button and bottom appbar
         child: Padding(
           padding: const EdgeInsets.fromLTRB(0, 0, 15, 0),
           child: Align(
